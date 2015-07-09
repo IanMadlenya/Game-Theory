ce48fc8d6af692c105c346241d29d4b933848578
小川慶将 <yshms_o@yahoo.co.jp>
2015-07-09 23:16:09 +0900

    Create KMR model, change a strategy one by one

--- StochasticEvolution/KMR.py
+++ StochasticEvolution/KMR.py
@@ -1,45 +1,150 @@
-# -*- coding: utf-8 -*-
+# coding:UTF-8
+"""
+Author:Yoshimasa Ogawa
+KMR (Kandori-Mailath-Rob) Model
+"""
+from __future__ import division
 import numpy as np
 import matplotlib.pyplot as plt
-from mc_tools import mc_compute_stationary, mc_sample_path
-from discrete_rv import DiscreteRV
-import random
-import fractions
+import quantecon as qe
+import gambit as gb
 
-payoff = np.array([[[4, 4], [0, 3]],
-                   [[3, 0], [2, 2]]])
-p_0 = np.array([[payoff[0][0][0], payoff[0][1][0]],
-                [payoff[1][0][0], payoff[1][1][0]]])
-n = 10
-t = 10000
-epsilon = 0.2
-r = random.uniform(0, 1)
-psi = (r, 1-r)
 
-P = np.zeros([n+1, n+1]) 
-for i in range(1, n+1): #行動１をとっている人が選ばれて行動０に変更する
-    num0 = fractions.Fraction(i-1, n-1)
-    num1 = fractions.Fraction(i, n)
-    ratio = np.array([1.0-num0, num0])
-    exp = np.dot(p_0, ratio)
-    if exp[0] > exp[1]:
-        P[i][i-1] = num1*((1.0-epsilon) + epsilon*0.5)
-    else:
-        P[i][i-1] = num1*(epsilon*0.5)
-for i in range(n): #行動0をとっている人が選ばれて行動1に変更する
-    num0 = fractions.Fraction(i, n-1)
-    num1 = fractions.Fraction(i, n)
-    ratio = np.array([1-num0, num0])
-    exp = np.dot(p_0, ratio)
-    if exp[0] < exp[1]:
-        P[i][i+1] = (1-num1)*((1-epsilon) + epsilon*0.5)
-    else:
-        P[i][i+1] = (1-num1)*(epsilon*0.5)
-for i in range(1, n): #行動を変えない
-    P[i][i] = 1 - P[i][i-1] - P[i][i+1]
-P[0][0] = 1 - P[0][1]
-P[n][n] = 1 - P[n][n-1]
+def kmr_markov_matrix(p, N, epsilon=0):
+    """
+    Generate the transition probability matrix for the KMR dynamics with
+    two acitons.
 
-X = mc_sample_path(P, psi, t)
-plt.plot(X)
-plt.show()
+    Parameters
+    ----------
+    p : scalar(int)
+        Probability on mixed strategy nash equilibrium.
+    N : scalar(int)
+        Number of players.
+    epsilon : scalar(int), optional(default=0)
+        perturbations.
+    Returns
+    ---------
+    P : ndarray(int, ndim=2)
+        The transition probability matrix for the KMR dynamics.
+    """
+    P = np.zeros((N+1, N+1), dtype=float)
+    P[0][1] = (epsilon) * (1/2)
+    P[N][N-1] = (epsilon) * (1/2)
+    P[0][0], P[N][N] = 1 - P[0][1], 1 - P[N][N-1]
+    for i in range(1, N):
+        P[i][i-1] = (i/N) * (epsilon * (1/2) + (1 - epsilon)*(((i - 1)/(N - 1) < p) + ((i - 1)/(N - 1) == p) * (1/2)))
+        P[i][i+1] = ((N-i)/N) * (epsilon * (1/2) + (1 - epsilon)*((i/(N - 1) > p) + (i/(N - 1) == p) * (1/2)))
+        P[i][i] = 1 - P[i][i-1] - P[i][i+1]
+    return P
+
+
+def compute_nash_equilibrium(payoff, same_payoff=False):
+    """
+    Generate the probabilities on mixed strategy nash equilibrium.
+
+    Parameters
+    ----------
+    payoff : array_like(float, ndim=2)
+        The payoff matrix.
+    same_payoff : True or False
+
+    Returns
+    -------
+    Nash : list
+        List of NashProfiles.
+    """
+    strategy = payoff.shape
+    g = gb.Game.new_table([strategy[0], strategy[1]])
+    for i in range(strategy[0]):
+        for j in range(strategy[1]):
+            if same_payoff is False:
+                for k in range(strategy[2]):
+                    g[i, j][k] = payoff[i][j][k]
+            else:
+                g[i, j][0] = payoff[i][j]
+                g[i, j][1] = payoff[j][i]
+    solver = gb.nash.ExternalEnumMixedSolver()
+    return solver.solve(g)
+
+
+class KMR(object):
+    """
+    Class representing the KMR dynamics with two actions.
+    """
+    def __init__(self, p, N, epsilon):
+        self.p = p
+        self.N = N
+        self.epsilon = epsilon
+        self.P = kmr_markov_matrix(p, N, epsilon)
+        self.mc = qe.MarkovChain(self.P)
+
+    def simulate(self, ts_length, init=None, num_reps=None, allow_return=False):
+        """
+        Simulate the dynamics.
+        Parameters
+        ----------
+        ts_length : scalar(int)
+            Length of each simulation.
+        init : scalar(int) or array_like(int, ndim=1),
+               optional(default=None)
+            Initial state(s). If None, the initial state is randomly
+            drawn.
+        num_reps : scalar(int), optional(default=None)
+            Number of simulations. Relevant only when init is a scalar
+            or None.
+        allow_return : True or False
+
+        Returns
+        -------
+        X : ndarray(int, ndim=1 or 2)
+            Array containing the sample path(s), of shape (ts_length,)
+            if init is a scalar (integer) or None and num_reps is None;
+            of shape (k, ts_length) otherwise, where k = len(init) if
+            init is an array_like, otherwise k = num_reps.
+        """
+        self.sample_path = self.mc.simulate(ts_length, init, num_reps)
+        self.ts_length = ts_length
+        if allow_return is True:
+            return self.sample_path
+
+    def compute_stationary_distribution(self, allow_return=False):
+        # mc.stationary_distributions の戻り値は2次元配列．
+        # 各行に定常分布が入っている (一般には複数)．
+        # epsilon > 0 のときは唯一，epsilon == 0 のときは複数ありえる．
+        # espilon > 0 のみを想定して唯一と決め打ちするか，
+        # 0か正かで分岐するかは自分で決める．
+        if self.epsilon != 0:
+            self.stationary_dist = self.mc.stationary_distributions[0]
+            if allow_return is True:
+                return self.stationary_dist  # これは唯一と決め打ちの場合
+        else:
+            print "epsilon is 0"
+
+    def plot_sample_path(self, ax=None, show=True):
+        fig, ax = plt.subplots()
+        ax.set_ylim(0, self.N)
+        ax.plot(self.sample_path)
+        plt.show()
+
+    def plot_stationary_distribution(self):
+        self.stationary_dist = self.mc.stationary_distributions[0]
+        fig, ax = plt.subplots()
+        ax.set_xlim(-0.5, self.N+0.5)
+        ax.set_ylim(0, 1.0)
+        ax.set_xlabel('State Space')
+        ax.set_ylabel('Probability')
+        ax.set_title('Stationary Distributions')
+        ax.bar(range(self.N+1), self.stationary_dist, align="center")
+        plt.show()
+
+    def plot_empirical_distribution(self):
+        fig, ax = plt.subplots()
+        n, bins = np.histogram(self.sample_path, bins=self.N+1, range=(0, self.N))
+        ax.set_xlim(-0.5, self.N+0.5)
+        ax.set_ylim(0, 1.0)
+        ax.set_xlabel('State Space')
+        ax.set_ylabel('Probability')
+        ax.set_title('Empirical Distributions')
+        ax.bar(range(self.N+1), n/self.ts_length, align="center")
+        plt.show()
